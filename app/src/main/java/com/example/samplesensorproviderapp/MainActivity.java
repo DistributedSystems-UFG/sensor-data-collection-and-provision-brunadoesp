7e4f8283e1798020753a2e13ea72902140d68df6
Bruna do Espirito Santo Sousa <brunadoesp@discente.ufg.br>
2023-12-23 16:37:27 -0300

    Alterações Android
    Inclusão da leitura de temperatura e publicação automática dos valores de temperatura e luminosidade

--- app/src/main/java/com/example/samplesensorproviderapp/MainActivity.java
+++ app/src/main/java/com/example/samplesensorproviderapp/MainActivity.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
-    public static final String brokerURI = "3.223.10.115";
+    public static final String brokerURI = "3.220.83.84";
 
     Activity thisActivity;
     TextView subMsgTextView;
